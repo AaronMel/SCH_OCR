5634eaedbf0fbb5fea479b50c695231c8a1d118c
AaronMel <67263769+AaronMel@users.noreply.github.com>
2020-11-30 08:13:16 +0100

    ocr porovnávání
    - přídáno porovnávání 2 filtrů

--- volumes/ocr/SolutionOCR.py
+++ volumes/ocr/SolutionOCR.py
@@ -18,21 +18,21 @@ def rectangle2(draw, x ,y ,l ,h, Outline):
     y1 = y + h
     draw.rectangle((x1, y1, x, y), outline=Outline)
     return draw
-    
+
 def chackNumberAndUnits(input, units):
     for unit in units:
-        a = "[0-9]+(" + unit + "\.*)$"   
+        a = "[0-9]+(" + unit + "\.*)$"
         if None != re.match(a, input.lower(), re.IGNORECASE):
             return unit
     return None
-    
+
 def chackUnits(input, units):
     for unit in units:
-        a = "(" + unit + ")$"   
+        a = "(" + unit + ")$"
         if None != re.match(a, input.lower(), re.IGNORECASE):
             return unit
     return None
-            
+
 def unitsMaker(units, prefixes):
     allUnits = []
     for unit in units:
@@ -40,26 +40,131 @@ def unitsMaker(units, prefixes):
         for prefixe in prefixes:
             allUnits.append(str(prefixe + unit))
     return allUnits
-        
-    
+
+
 
 class SolutionOCR:
     def __init__(self, language, minConfPts):
         self.language = language
         self.minConfPts = minConfPts
 
-    def OCR(self, imageFile):
+    def OCR(self, imageFile, filter=True, filterType="gauss"):
         base = {
-            "data": []
+            "data": [],
+            "dataGauss": [],
+            "dataCont": [],
+            "dataConfirmed": []
         }
 
         image = Image.open(imageFile)
-        imageForOCR = image.copy() # 813
+        imageForOCR = image.copy()
+        if filter:
         # Filters for ocr (blurs)
-        gaussImage = imageForOCR.filter(ImageFilter.GaussianBlur(1)) # 828 confPts: avg. 93
-        boxImage = imageForOCR.filter(ImageFilter.BoxBlur(5))
-        blurImage = imageForOCR.filter(ImageFilter.BLUR) # 836
-        results = pytesseract.image_to_data(gaussImage, lang=self.language, output_type=Output.DICT)
+            pass
+            """
+            if filterType == "gauss":
+
+            elif filterType == "bblur":
+                boxImage = imageForOCR.filter(ImageFilter.BoxBlur(1))
+                designatedImage = boxImage
+            elif filterType == "blur":
+                blurImage = imageForOCR.filter(ImageFilter.BLUR)
+                designatedImage = blurImage
+            elif filterType == "cont":
+
+            elif filterType == "find":
+                findImage = imageForOCR.filter(ImageFilter.FIND_EDGES)
+                designatedImage = findImage
+            elif filterType == "edge":
+                edgeImage = imageForOCR.filter(ImageFilter.EDGE_ENHANCE_MORE)
+                designatedImage = edgeImage
+        else:
+            designatedImage = imageForOCR
+            """
+        gaussImage = imageForOCR.filter(ImageFilter.GaussianBlur(1))
+        resultsGauss = pytesseract.image_to_data(gaussImage, lang=self.language, output_type=Output.DICT)
+
+        contImage = imageForOCR.filter(ImageFilter.CONTOUR)
+        resultsCont = pytesseract.image_to_data(contImage, lang=self.language, output_type=Output.DICT)
+
+        for a in range(0, len(resultsGauss["text"])):
+            xcoordsGauss = resultsGauss["left"][a]
+            ycoordsGauss = resultsGauss["top"][a]
+            widthGauss = resultsGauss["width"][a]
+            heightGauss = resultsGauss["height"][a]
+
+            textGauss = resultsGauss["text"][a]
+            confPtsGauss = int(resultsGauss["conf"][a])
+
+            object = {
+                "codeName": str(a),
+                "x": xcoordsGauss,
+                "y": ycoordsGauss,
+                "w": widthGauss,
+                "h": heightGauss,
+                "ocrText": textGauss,
+                "rawOcrText": textGauss,
+                "confPts": confPtsGauss,
+                "typeOfObject": "unknown"
+            }
+            base["dataGauss"].append(object)
+
+            for b in range(0, len(resultsCont["text"])):
+                xcoordsCont = resultsCont["left"][b]
+                ycoordsCont = resultsCont["top"][b]
+                widthCont = resultsCont["width"][b]
+                heightCont = resultsCont["height"][b]
+
+                textCont = resultsCont["text"][b]
+                confPtsCont = int(resultsCont["conf"][b])
+
+                if confPtsGauss > self.minConfPts:
+                    if xcoordsGauss == xcoordsCont:
+                        if ycoordsGauss == ycoordsCont:
+                            if textGauss == textCont:
+                                confirText = textGauss
+                                confirX = xcoordsGauss
+                                confirY = ycoordsGauss
+                                confirW = widthGauss
+                                confirH = heightGauss
+
+                                object = {
+                                    "codeName": str(a),
+                                    "x": confirX,
+                                    "y": confirY,
+                                    "w": confirW,
+                                    "h": confirH,
+                                    "ocrText": confirText,
+                                    "rawOcrText": confirText,
+                                    "confPts": 100,
+                                    "typeOfObject": "unknown"
+                                }
+                                base["dataConfirmed"].append(object)
+
+        for i in range(0, len(resultsCont["text"])):
+            xcoordsCont = resultsCont["left"][i]
+            ycoordsCont = resultsCont["top"][i]
+            widthCont = resultsCont["width"][i]
+            heightCont = resultsCont["height"][i]
+
+            textCont = resultsCont["text"][i]
+            confPtsCont = int(resultsCont["conf"][i])
+
+            object = {
+                "codeName": str(i),
+                "x": xcoordsCont,
+                "y": ycoordsCont,
+                "w": widthCont,
+                "h": heightCont,
+                "ocrText": textCont,
+                "rawOcrText": textCont,
+                "confPts": confPtsCont,
+                "typeOfObject": "unknown"
+            }
+            base["dataCont"].append(object)
+
+        return base
+        """
         for i in range(0, len(results["text"])):
             xcoords = results["left"][i]
             ycoords = results["top"][i]
@@ -70,7 +175,7 @@ class SolutionOCR:
             confPts = int(results["conf"][i])
 
             rawOcrText = results["text"][i]
-            
+
             if confPts > self.minConfPts:
                 object = {
                     "codeName": str(i),
@@ -85,26 +190,25 @@ class SolutionOCR:
                 }
                 base["data"].append(object)
 
-                #debugImage(imageFile, ocrText, xcoords, ycoords, i)
         return base
-        
-    
-        
-        
+        """
+
+
+
     def filter(self, base):
         keyUnits = ["m","g","s"]
         prefixes = ["k","d","c","m" ]
         units = ["ks","PCS","n.","č.","h","m","d"]
-        allUnits = units + unitsMaker(keyUnits, prefixes) 
+        allUnits = units + unitsMaker(keyUnits, prefixes)
         print (allUnits)
         currencies = ["kč","czk","eur","eu","usd","gbp","$","€","£"]
-        
+
         dictionary = enchant.Dict("cs")
         output = {
             "data": []
         }
-        
-        for record in base["data"]:
+
+        for record in base["dataGauss"]:
             a = 60
             b = 35
             rawOcrText = record["rawOcrText"]
@@ -190,16 +294,102 @@ class SolutionOCR:
                             record["ocrText"] = rawOcrText
                             output["data"].append(record)
                             print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
-                
-                
-                
-                
-                
+        for record in base["dataConfirmed"]:
+            a = 60
+            b = 35
+            rawOcrText = record["rawOcrText"]
+            ID = int(record["codeName"])
+            confPts = record["confPts"]
+            if rawOcrText.isspace() or not rawOcrText:
+                print(str(ID) + " - " + str(record["confPts"]) + " - " + "\" \" - "+ str(record["rawOcrText"]))
+                pass
+            elif None != re.match('(\ *\.\.\.\.+\ *)', rawOcrText):
+                print(str(ID) + " - " + str(record["confPts"]) + " - " + "\"..\" - "+ str(record["rawOcrText"]))
+                pass
+            elif None != re.match('(\ *\—\—+\ *)', rawOcrText):
+                print(str(ID)+ " - " + str(record["confPts"]) + " - " + "\"——\" - "+ str(record["rawOcrText"]))
+                pass
+            elif None != re.match('(\ *\-\-+\ *)', rawOcrText):
+                print(str(ID) + " - " + str(record["confPts"]) + " - " + "\"--\" - "+ str(record["rawOcrText"]))
+                pass
+            elif None != re.match('(\ *\|+\ *)', rawOcrText):
+                print(str(ID) + " - " + str(record["confPts"]) + " - " + "\"|\" - "+ str(record["rawOcrText"]))
+                pass
+            else:
+                if  None != chackNumberAndUnits(rawOcrText, allUnits):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "number and unit"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif  None != chackUnits(rawOcrText, allUnits):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "unit"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif  None != chackUnits(rawOcrText, currencies):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "currencie"
+                    output["data"].append(record)
+                elif None != re.match('[0-9][0-9](:[0-9][0-9])+', rawOcrText):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "time"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif None != re.match('[0-9][0-9][\.\/][0-9][0-9][\.\/][0-9][0-9]+', rawOcrText):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "date"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif None != re.match('[0-9]+[\.\,][0-9]+', rawOcrText):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "decimal number"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif None != re.match('[0-9]+[\.\,][0-9]+', rawOcrText):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "decimal number"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                elif None != re.match('[0-9]+', rawOcrText):
+                    record["ocrText"] = rawOcrText
+                    record["typeOfObject"] = "integer number"
+                    output["data"].append(record)
+                    print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                else:
+                    ocrTextClear = ""
+                    for character in rawOcrText:
+                        if character.isalnum():
+                            ocrTextClear += character
+                    if ocrTextClear != "":
+                        if  None != chackUnits(ocrTextClear, allUnits):
+                            record["ocrText"] = rawOcrText
+                            record["typeOfObject"] = "unit"
+                            output["data"].append(record)
+                            print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                        elif  None != chackUnits(ocrTextClear, currencies):
+                            record["ocrText"] = rawOcrText
+                            record["typeOfObject"] = "currencie"
+                            output["data"].append(record)
+                            print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                        elif dictionary.check(ocrTextClear):
+                            record["ocrText"] = rawOcrText
+                            record["typeOfObject"] = "word"
+                            output["data"].append(record)
+                            print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+                        else:
+                            record["ocrText"] = rawOcrText
+                            output["data"].append(record)
+                            print(str(ID) + " - " + str(record["confPts"]) + " - " + str(record["typeOfObject"]) + " - "+ str(record["ocrText"]))
+
+
+
+
+
                 # ocrTextClear = ""
                 # for character in rawOcrText:
                 #     if character.isalnum():
                 #         ocrTextClear += character
-                # if ocrTextClear != "":       
+                # if ocrTextClear != "":
                 #     if dictionary.check(ocrTextClear):
                 #         record["confPts"] = 100
                 #         record["ocrText"] = rawOcrText
@@ -228,13 +418,13 @@ class SolutionOCR:
                 #         record["typeOfObject"] = 5
                 #         output["data"].append(record)
                 #         print(str(ID) + " - " + str(record["typeOfObject"]) + " - "+ str(record["rawOcrText"]) + " - "+ str(record["ocrText"]))
-                # 
-                # 
-                
+                #
+                #
+
         print("finish")
         return output
-            
-            
+
+
 
 def imageDebug(imageFile, dictionary, path):
     baseImage = Image.open(imageFile)
@@ -280,8 +470,8 @@ def imageDebug(imageFile, dictionary, path):
             color = ping
         elif type == "currencie":
             color = purple
-            
-        
+
+
 
         draw = rectangle2(draw, xcoords, ycoords, width, height, color)
         draw.text((xcoords, ycoords), textImage + "\n" + ID, font=font, fill=color)
--- volumes/ocr/main.py
+++ volumes/ocr/main.py
@@ -1,20 +1,122 @@
 from SolutionOCR import SolutionOCR, imageDebug
 
-ImageFile = "./input/Seznam.jpg"
-output_path1 = './output/imageDebugv1.jpg'
-output_path2 = './output/imageDebug2.jpg'
+ImageFile1 = "./input/Seznam.jpg"
+ImageFile2 = "./input/uctenka.jpg"
+
+# basic image output not refined
+output_basic = './output/basicImageDebug.jpg'
+
+output_basic1 = './output/basicImageDebug1.jpg'
+output_basic2 = './output/basicImageDebug2.jpg'
+output_basic3 = './output/basicImageDebug3.jpg'
+output_basic4 = './output/basicImageDebug4.jpg'
+output_basic5 = './output/basicImageDebug5.jpg'
+output_basic6 = './output/basicImageDebug6.jpg'
+# refined image output all languages optimized
+output_dict = './output/dictImageDebug.jpg'
+
+output_dict1 = './output/dictImageDebug1.jpg'
+output_dict2 = './output/dictImageDebug2.jpg'
+output_dict3 = './output/dictImageDebug3.jpg'
+output_dict4 = './output/dictImageDebug4.jpg'
+output_dict5 = './output/dictImageDebug5.jpg'
+output_dict6 = './output/dictImageDebug6.jpg'
+
 
 ocr_cz = SolutionOCR("ces", 5)
-output = ocr_cz.OCR(ImageFile)
-imageDebug(ImageFile, output, output_path1)
+output = ocr_cz.OCR(ImageFile1)
+imageDebug(ImageFile1, output, output_basic)
 output = ocr_cz.filter(output)
-imageDebug(ImageFile, output, output_path2)
-
-
+imageDebug(ImageFile1, output, output_dict)
 
 outputTextFile_path = './output/imageLog.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+
+"""
+# GaussianBlur
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1)
+imageDebug(ImageFile1, output, output_basic1)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict1)
+
+outputTextFile_path = './output/imageLog1.txt'
 textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
 for i in range(len(output["data"])):
     # print(str(output["data"][i]))
     textFile.write(str(output["data"][i]) + '\n')
 textFile.close()
+
+# BoxBlur
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1, filterType="bblur")
+imageDebug(ImageFile1, output, output_basic2)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict2)
+
+outputTextFile_path = './output/imageLog2.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    # print(str(output["data"][i]))
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+
+# blur
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1, filterType="blur")
+imageDebug(ImageFile1, output, output_basic3)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict3)
+
+outputTextFile_path = './output/imageLog3.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    # print(str(output["data"][i]))
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+
+# contour
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1, filterType="cont")
+imageDebug(ImageFile1, output, output_basic4)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict4)
+
+outputTextFile_path = './output/imageLog4.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    # print(str(output["data"][i]))
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+
+# find edges
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1, filterType="find")
+imageDebug(ImageFile1, output, output_basic5)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict5)
+
+outputTextFile_path = './output/imageLog5.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    # print(str(output["data"][i]))
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+
+# edge enhance more
+ocr_cz = SolutionOCR("ces", 5)
+output = ocr_cz.OCR(ImageFile1, filterType="edge")
+imageDebug(ImageFile1, output, output_basic6)
+output = ocr_cz.filter(output)
+imageDebug(ImageFile1, output, output_dict6)
+
+outputTextFile_path = './output/imageLog6.txt'
+textFile = open(outputTextFile_path, 'w+', encoding='utf-8')
+for i in range(len(output["data"])):
+    # print(str(output["data"][i]))
+    textFile.write(str(output["data"][i]) + '\n')
+textFile.close()
+"""
